90770a69c63e73b6309df0aefe23ccd3eaf7de12
jaskaran-23 <karanstar2397@gmail.com>
2021-03-18 18:30:35 +0530

    Crypto Wallet added

--- models/Wallet.py
+++ models/Wallet.py
@@ -1,72 +1,132 @@
-from pymongo import MongoClient
-from pprint import pprint
 import pymongo
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 class Wallet:
 
-	def __init__(self,email):
+	def __init__(self,email,data):
+		# if calling through object we need to have these 2 things.
 		self.email=email
-	
-	@classmethod
-	def retrieve_wallet(cls,email):
+		self.data=data
+
+	def insert(self):
+		# Inserting new document in our collection.
+		#Using pymongo to connect to the mongodb.
+		#Now we will establish connection on default port num 27017.
 		client=MongoClient('localhost',27017)
-		collection=db['wallet_user']
-		ans=collection.find_one({'email':email})
-		client.close()
-		if ans is not None:
-			return cls(ans['BTC'],ans['ETH'],ans['LTC'])
-		else:
-			return None,400
-	
-	@classmethod
-	def retrieve_btc(cls,email):
-		client=MongoClient('localhost',27017)
-		collection=db['wallet_user']
-		ans=collection.find_one({'email':email})
-		client.close()
-		if ans is not None:
-			return cls(ans['BTC'])
-		else:
-			return None,400
-			
-	@classmethod
-	def retrieve_eth(cls,email):
-		client=MongoClient('localhost',27017)
-		collection=db['wallet_user']
-		ans=collection.find_one({'email':email})
-		client.close()
-		if ans is not None:
-			return cls(ans['ETH'])
-		else:
-			return None,400
-			
-	@classmethod
-	def retrieve_ltc(cls,email):
-		client=MongoClient('localhost',27017)
-		collection=db['wallet_user']
-		ans=collection.find_one({'email':email})
-		client.close()
-		if ans is not None:
-			return cls(ans['LTC'])
-		else:
-			return None,400
-			
-		
-	def insert(self,btc,eth,ltc):
-		client=MongoClient('localhost',27017)
-		collection=db['wallet_user']
+		db=client['test-user-db-compra-venta']
+		collection=db['test-wallet-collection']
+		email=self.email
+		data=self.data
+
+		#post in the form of json/dictionary.
+
 		post={
-			'btc':btc,
-			'eth':eth,
-			'ltc':ltc
-			}
+
+			'email':email
+			'data':data
+
+		}
+
+		client.close()
+
 		try:
-			if collection.insert_one(post) is not None:
-				client.close()
-				return True
-			else:
-				client.close()
-				return False
+			collection.insert_one(post)
+			
+			return True
+		except:
+			return False
+
+
+	
+	#Creating class method because we don't want to call through object.
+	# we want to retrieve data thrugh email
+
+	@classmethod
+	def find_by_email(cls,email):
+
+		client=MongoClient()
+		db=client['test-user-db-compra-venta']
+		collection=db['test-wallet-collection']
+		# finding by making email as a key which will definately be unique to all users.
+		ans=collection.find_one({'email':email})
+		client.close()
+		if ans is not None:
+			return cls(ans['email'],ans['data']),ans['_id']
+			
+		else:
+			
+			return None,None
+			
+
+
+	@classmethod
+	def find_by_email_and_cryptocurrency(cls,email,cryptocurrency):
+
+		client=MongoClient()
+		db=client['test-user-db-comra-venta']
+		collection=db['test-wallet-collection']
+		# getting information of a paticular currency.
+		ans=collection.find_one({'email':email})
+		client.close()
+		if ans is not None:
+
+			return ans['data'].get(cryptocurrency,None)
+		else:
+			return None
+	
+
+	@classmethod
+	def reset_wallet(cls,email):
+
+		client=MongoClient()
+		db=client['test-user-db-compra-venta']
+		collection = db['test-wallet-collection']
+		
+
+		reset_data={
+
+			'USD':1000000.000000
+
+		}
+
+		try:
+			collection.update({'email':email},{"$set":{"data":reset_data}})
+			client.close()
+			return 1
+
 		except:
 			client.close()
-			return False
+			return None
+
+	@classmethod
+
+	def transaction(cls,email,base_asset,quote_asset,amount1,amount2):
+
+		client=MongoClient()
+		db=client['test-user-db-compra-venta']
+		collection = db['test-wallet-collection']
+
+		user_key={'email':email}
+		user_value={'$set':{base_asset:amount1}}
+
+		user_value1={'$set':{quote_asset:amount2}}
+		
+		try:
+			collection.update(user_key,user_value)
+			collection.update(user_key,user_value1)
+			
+
+		except:
+			return None
+
+
+
+
+
+
+
+
+
+
+
