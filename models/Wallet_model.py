d6ef4c096a128c1e1de612ed5b2bf213ab88ac2a
Daksh Verma <dakshverma.verma@gmail.com>
2021-04-24 17:18:37 +0530

    fixed issues

--- models/Wallet_model.py
+++ models/Wallet_model.py
@@ -12,16 +12,11 @@ class Wallet:
 
 
 	@classmethod
-<<<<<<< HEAD
 	def check_balance(cls,email,coin,amount):
-=======
-	def check_balance(self,email,coin,amount):
->>>>>>> 996a65a89312450173c1e72eb861bfcf61e1e173
 
 		client=MongoClient('localhost',27017)
 		db=client['test-user-db-compra-venta']
 		collection=db['test-user-wallet']
-<<<<<<< HEAD
 		try:
 			result=collection.find_one({'email':email})
 			client.close()
@@ -44,33 +39,10 @@ class Wallet:
 
 		client=MongoClient('localhost',27017)
 		db=client['test-user-db-compra-venta']
-=======
-
-		result=collection.find_one({'email':email})
-		client.close()
-		if result is not none:
-			bal=result['balance']
-			currency_amt=bal['coin']
-			if currency_amt >= amount:
-				# true
-				return True
-			else:
-				return False
-		else:
-			return False
-
-
-	@classmethod
-	def increase_balance_currency_amt(self,email,coin,amount):
-
-		client=MongoClient('localhost',27017)
-		db=collection['test-user-db-compra-venta']
->>>>>>> 996a65a89312450173c1e72eb861bfcf61e1e173
 		collection=db['test-user-wallet']
 
 
 		result=collection.find_one({'email':email})
-<<<<<<< HEAD
 		try:
 			if result is not None:
 				bal=result['balance']
@@ -117,50 +89,11 @@ class Wallet:
         
 		client=MongoClient('localhost',27017)
 		db=client['test-user-db-compra-venta']
-=======
-		client.close()
-		try:
-			if result is not none:
-				bal=result['balance']
-				bal[coin]+=amount
-				return True
-			else:
-				return False
-		except:
-			return False
-
-
-	@classmethod
-	def decrease_balance_currency_amt(self,email,coin,amount):
-
-		client=MongoClient('localhost',27017)
-		db=collection['test-user-db-compra-venta']
-		collection=db['test-user-wallet']
-
-
-		result=collection.find_one({'email':email})
-		client.close()
-		if result is not none:
-			bal=result['balance']
-			bal[coin]-=amount
-			return True
-		else:
-			return False
-
-	
-	@classmethod
-	def reset_account(self,email):
-        
-		client=MongoClient('localhost',27017)
-		db=collection['test-user-db-compra-venta']
->>>>>>> 996a65a89312450173c1e72eb861bfcf61e1e173
 		collection=db['test-user-wallet']
 
 		my_query={'email':email}
 
 		new_value={
-
-<<<<<<< HEAD
 				'$set':{
 				'balance':{
 					"BTC":0.00000000,
@@ -181,37 +114,11 @@ class Wallet:
 				}
 			}
 		}
-=======
-		'$set':{
-		'balance':{
-        "BTC":0.00,
-        "ETH":0.00,
-        "LTC":0.00,
-        "XRP":0.00,
-        "BNB":0.00,
-        "USDT":50000.00
-    	},
-
-    	'fixed_balance':{
-        "BTC":0.00,
-        "ETH":0.00,
-        "LTC":0.00,
-        "XRP":0.00,
-        "BNB":0.00,
-        "USDT":0.00
-    	}
-
-
-    }
-
- }
->>>>>>> 996a65a89312450173c1e72eb861bfcf61e1e173
  		
 		try:
  			collection.update_one(my_query,new_value)
  			collection.update_one(my_query,new_fixed_value)
  			client.close()
-<<<<<<< HEAD
  			return 1
 		except:
  			client.close()
@@ -224,25 +131,9 @@ class Wallet:
 
 		client=MongoClient('localhost',27017)
 		db=client['test-user-db-compra-venta']
-=======
- 			return True
-		except:
- 			client.close()
- 			return False
-
-
-	@classmethod
-	def make_user_wallet(self,email):
-
-
-		client=MongoClient('localhost',27017)
-		db=collection['test-user-db-compra-venta']
->>>>>>> 996a65a89312450173c1e72eb861bfcf61e1e173
 		collection=db['test-user-wallet']
 
 		post={
-
-<<<<<<< HEAD
 			"email":email,
 			"balance":{
 				"BTC":0.00000000,
@@ -300,69 +191,10 @@ class Wallet:
 
 		client=MongoClient('localhost',27017)
 		db=client['test-user-db-compra-venta']
-=======
-    "email":email,
-    "balance":{
-        "BTC":0.00,
-        "ETH":0.00,
-        "LTC":0.00,
-        "XRP":0.00,
-        "BNB":0.00,
-        "USDT":50000.00
-    },
-    "fixed_balance":{
-        "BTC":0.00,
-        "ETH":0.00,
-        "LTC":0.00,
-        "XRP":0.00,
-        "BNB":0.00,
-        "USDT":0.00
-    }
-}
-		try:
-			if collection.insert_one(post) == None:
-				client.close()
-				return False
-			else:
-				client.close()
-				return True
-		except:
-			client.close()
-			return False
-
-
-	@classmethod
-	def increase_fixed_balance_currency_amt(self,email,coin,amount):
-
-		client=MongoClient('localhost',27017)
-		db=collection['test-user-db-compra-venta']
 		collection=db['test-user-wallet']
 
 
 		result=collection.find_one({'email':email})
-		client.close()
-		try:
-			if result is not none:
-				bal=result['fixed_balance']
-				bal[coin]+=amount
-				return True
-			else:
-				return False
-		except:
-			return False
-
-
-	@classmethod
-	def decrease_fixed_balance_currency_amt(self,email,coin,amount):
-
-		client=MongoClient('localhost',27017)
-		db=collection['test-user-db-compra-venta']
->>>>>>> 996a65a89312450173c1e72eb861bfcf61e1e173
-		collection=db['test-user-wallet']
-
-
-		result=collection.find_one({'email':email})
-<<<<<<< HEAD
 		try:
 			if result is not None:
 				bal=result['fixed_balance']
@@ -428,48 +260,3 @@ class Wallet:
 		except:
 			client.close()
 			return -1
-=======
-		client.close()
-		try:
-			if result is not none:
-				bal=result['fixed_balance']
-				bal[coin]-=amount
-				return True
-			else:
-				return False
-		except:
-			return False
-
-
-	@classmethod
-	def get_wallet(self,email):
-
-		
-		client=MongoClient('localhost',27017)
-		db=collection['test-user-db-compra-venta']
-		collection=db['test-user-wallet']
-
-
-		result=collection.find_one({'email':email})
-		if result:
-			return cls(result['email'], result['balance'], result['fixed_balance']), str(result['_id'])
-		else:
-			return None, None
-
-
-	@classmethod
-	def get_wallet_currency(self,email,coin):
-
-		
-		client=MongoClient('localhost',27017)
-		db=collection['test-user-db-compra-venta']
-		collection=db['test-user-wallet']
-
-
-		result=collection.find_one({'email':email})
-		if result:
-			return cls(result['email'], result['balance'][coin]), str(result['_id'])
-		else:
-			return None, None
-
->>>>>>> 996a65a89312450173c1e72eb861bfcf61e1e173
