d393c85142a62f3f072c32bc85a509c9e8c16c5d
daksh <dakshverma.verma@gmail.com>
2021-04-30 00:30:56 +0530

    env variables added for heroku

---
+++ utils/config.py
@@ -0,0 +1,7 @@
+import os
+email_password = os.environ.get("email_password")
+api_key = os.environ.get("api_key")
+secret_key = os.environ.get("secret_key")
+db_url = os.environ.get("db_url")
+redis_url = os.environ.get("redis_url")
+redis_password = os.environ.get("redis_password")
