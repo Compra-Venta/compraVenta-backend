fbdca06baec9d94123905dc91fa5ffb2b0cbb90b
daksh <dakshverma.verma@gmail.com>
2021-05-01 11:34:52 +0530

    changed refresh token expiry

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ from datetime import timedelta
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "somesecretcode"
 
-ACCESS_EXPIRES= timedelta(hours=1)
+ACCESS_EXPIRES= timedelta(hours=2)
 
 app.config["JWT_ACCESS_TOKEN_EXPIRES"]=ACCESS_EXPIRES
-app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(minutes=1)
+app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days = 30)
 app.config['JWT_BLACKLIST_ENABLED'] = True
 jwt = JWTManager(app)
 
